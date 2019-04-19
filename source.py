ef790b688910ae90029ded50c57f08a8ebcd54d3
Mylyanyk Marko <marko@Markos-MacBook-Pro.local>
2019-04-19 18:41:02 +0300

    Ready grammar for parsing procedure

--- source.py
+++ source.py
@@ -2,38 +2,71 @@ from lark import Lark, Transformer
 
 grammar = """
     start: instruction+
-    instruction: repeat | print
-    
+    instruction: repeat | print | declare | procedure | step | conferment | comparator
+
+    procedure: "procedure" step* "end procedure"
+    step: "initiate and confirm step" name activity "end step;"
     repeat: "repeat" NUMBER instruction
-    print: "print" STRING
-    STRING: /'[^']*'/
-    
+    print: "log" double_quoted ";"
+    STRING: /[a-zA-Z0-9_.-]{2,}/
+    declare: "declare" variable* "end declare"
+    variable: "variable" name "of type" type
+    conferment: name ":=" double_quoted ";"
+    comparator: "if value of" name "!=" name "then" activity "end if;"
+    double_quoted: /"[^"]*"/
+    name: STRING
+    type: STRING
+    activity: instruction*
+
     %import common.INT -> NUMBER
     %import common.WS
     %ignore WS
+    %ignore ","
 """
-
-parser = Lark(grammar)
 
 
 class MyTransformer(Transformer):
-    def repeat(self, matches):
-        return f"for _ in range({matches[0]}):\n\t" + "\n\t".join(matches[1].split('\n'))
+    def declare(self, matches):
+        return matches[0]
 
-    def print(self, matches):
-        return f"print({matches[0]})"
+    def instruction(self, matches):
+        return matches[0]
+
+    def variable(self, matches):
+        return f"{matches[0]} = {matches[1]}"
+
+    def name(self, matches):
+        return matches[0]
+
+    def type(self, matches):
+        if matches[0] == "integer":
+            return "int()"
+        elif matches[0] == "string":
+            return "str()"
+
+    def procedure(self, matches):
+        return matches
+
+    def step(self, matches):
+        return matches
+
+    def conferment(self, matches):
+        return matches
+
+    def comparator(self, matches):
+        return matches
 
     def start(self, matches):
         return "\n".join(matches)
 
-    def instruction(self, commands):
-        return commands[0]
 
+parser = Lark(grammar)
 
 with open("in.pluto") as f:
     text = "\n".join(f.readlines()).strip()
 
 tree = parser.parse(text)
+#print(tree)
 
-new_tree = MyTransformer().transform(tree)
-print(new_tree)
+p_code = MyTransformer().transform(tree)
+print(p_code)
